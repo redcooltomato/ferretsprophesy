c925bfdfc0496f4166eda239b445045adfe02e6e
redcooltomato <redcooltomato@gmail.com>
2026-01-31 00:19:46 +0300

    timed input initial (buggy)

--- src/cross.rs
+++ src/cross.rs
@@ -1,12 +1,22 @@
 use crossterm::{
-    QueueableCommand, cursor, event::{self, Event, KeyCode}, style::{self, Print, StyledContent, Stylize}, terminal::{self, Clear, ClearType}
+    QueueableCommand, cursor, 
+    event::{self, Event, KeyCode, KeyEvent, KeyEventKind, KeyModifiers, poll, read}, 
+    style::{self, Print, StyledContent, Stylize}, terminal::{self, Clear, ClearType}
 };
-use std::io::{Result, Write, stdout};
+use std::{
+    io::{Result, Write, stdout}, 
+    thread, 
+    time::{self, Duration, SystemTime}, 
+    cmp
+};
 use rand::Rng;
 
-const INITIAL_SNEK_LEN: u16 = 3;
 
-#[derive(Debug, Clone, Copy)]
+const INITIAL_SNEK_LEN: u16 = 3;
+const INPUT_WAIT_TIME: u64 = 1000;
+
+
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum Cell {
     BorderCell,
     SnekCell(u16),
@@ -26,6 +36,8 @@ impl Cell {
     }
 }
 
+
+#[derive(Debug, Clone)]
 pub struct MapTemplate {
     map: Vec<Vec<Cell>>,
     headx: u16,
@@ -33,15 +45,22 @@ pub struct MapTemplate {
 }
 
 
+
 pub fn crossrender(info: &mut MapTemplate) -> Result<()> {
     let MapTemplate { map, headx, heady } = info;
     let (h, w) : (u16, u16) = (map.len() as u16, map[0].len() as u16);
     let mut sneklen = INITIAL_SNEK_LEN;
 
+    let mut prev_key: KeyCode = KeyCode::Up;
+    let mut just_eaten: bool = false;
+    let mut now: SystemTime;
+    let mut wait: Duration;
+
     terminal::enable_raw_mode()?;
     let mut stdout = stdout();
 
-    loop {
+
+    'main_loop: loop {
         stdout.queue(terminal::Clear(ClearType::All))?;
 
         for row in 0..h {
@@ -49,23 +68,49 @@ pub fn crossrender(info: &mut MapTemplate) -> Result<()> {
                 stdout
                     .queue(cursor::MoveTo(row * 2, cell * 2))?
                     .queue(style::PrintStyledContent(get_cell_styled(&map[row as usize][cell as usize], &sneklen)))?;
-                map[row as usize][cell as usize].decrease_thouself();
+
+                if !just_eaten {
+                    map[row as usize][cell as usize].decrease_thouself();
+                }
             }
         }
 
         stdout.flush()?;
+        
+        loop {
+            now = SystemTime::now();
 
-        if let Event::Key(key_event) = event::read()? {
-            match key_event.code {
-                KeyCode::Char('q') => break,
+            if poll(time::Duration::from_millis(INPUT_WAIT_TIME))? {
+                if let Event::Key(key_event) = event::read()? {
+                    if key_event.kind == KeyEventKind::Release {
+                        continue;
+                    }
+                    match key_event.code {
+                        KeyCode::Char('q') => break 'main_loop,
 
-                KeyCode::Left | KeyCode::Right | KeyCode::Up | KeyCode::Down 
-                    => handlekey(key_event.code, map, headx, heady, &mut sneklen),
+                        KeyCode::Left | KeyCode::Right | KeyCode::Up | KeyCode::Down 
+                            => { handlekey(key_event.code, map, headx, heady, &mut sneklen, &mut just_eaten); prev_key = key_event.code; },
 
-                _ => continue,
+                        _ => handlekey(prev_key, map, headx, heady, &mut sneklen, &mut just_eaten),
+                    }
+                } 
+                
+                else {
+                    handlekey(prev_key, map, headx, heady, &mut sneklen, &mut just_eaten);
+                }
+            } 
+            
+            else {
+                handlekey(prev_key, map, headx, heady, &mut sneklen, &mut just_eaten);
             }
+            
+            wait = Duration::from_millis(INPUT_WAIT_TIME) - cmp::min(SystemTime::now().duration_since(now).expect("timer handling err"), Duration::from_millis(INPUT_WAIT_TIME));
+            /* dbg!(wait.as_millis()); */
+            thread::sleep(wait);
+            break;
         }
     }
+
 
     stdout.queue(terminal::Clear(ClearType::All))?;
     stdout.queue(cursor::MoveTo(0, 0))?;
@@ -74,6 +119,7 @@ pub fn crossrender(info: &mut MapTemplate) -> Result<()> {
     terminal::disable_raw_mode()?;
     Ok(())
 }
+
 
 
 fn get_cell_styled(c: &Cell, sneklen: &u16) -> StyledContent<char> {
@@ -89,42 +135,48 @@ fn get_cell_styled(c: &Cell, sneklen: &u16) -> StyledContent<char> {
 }
 
 
-fn handlekey(key: KeyCode, map: &mut Vec<Vec<Cell>>, headx: &mut u16, heady: &mut u16, sneklen: &mut u16) {
-    match key { // todo add apple handling
+fn handlekey(key: KeyCode, map: &mut Vec<Vec<Cell>>, headx: &mut u16, heady: &mut u16, sneklen: &mut u16, just_eaten: &mut bool) {
+    match key {
         KeyCode::Left => {
             if *headx > 1 {
                 *headx -= 1;
             } else {
                 *headx = (map.len() as u16) - 2;
             }
-            map[*headx as usize][*heady as usize] = Cell::SnekCell(*sneklen);
         },
+
         KeyCode::Right => {
             if *headx < (map.len() as u16) - 2 {
                 *headx += 1;
             } else {
                 *headx = 1;
             }
-            map[*headx as usize][*heady as usize] = Cell::SnekCell(*sneklen);
         }
+
         KeyCode::Up => {
             if *heady > 1 {
                 *heady -= 1;
             } else {
                 *heady = (map[0].len() as u16) - 2;
             }
-            map[*headx as usize][*heady as usize] = Cell::SnekCell(*sneklen);
         }
+
         KeyCode::Down => {
             if *heady < (map[0].len() as u16) - 2 {
                 *heady += 1;
             } else {
                 *heady = 1;
             }
-            map[*headx as usize][*heady as usize] = Cell::SnekCell(*sneklen);
         }
+
         _ => unreachable!(),
     }
+
+    // assume _ arm is really unreachable()
+    if map[*headx as usize][*heady as usize] == Cell::AppleCell {
+        *just_eaten = true;
+    }
+    map[*headx as usize][*heady as usize] = Cell::SnekCell(*sneklen);
 }
 
 
